64c456bf3df2899d92f3d8c5b4367ff3539f4943
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-20 10:23:44 -0300

    Now enemies waves spawn indefinitely

--- Assets/Resources/Scripts/EnemySpawner.cs
+++ Assets/Resources/Scripts/EnemySpawner.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
-{
+{    
     [SerializeField] private List<WaveConfigSO> waveConfigs;
     [SerializeField] private float timeBetweenWaves = 0f;
+    [SerializeField] private bool isLooping;
     private WaveConfigSO currentWave;
 
     void Start()
@@ -25,23 +26,29 @@ public class EnemySpawner : MonoBehaviour
 
     //Spawn all the enemies of this wave
     private IEnumerator SpawnEnemies()
-    {
-        foreach (WaveConfigSO wave in waveConfigs)
+    {        
+        //While makes the code runs indefinitely while the condition is true
+        //do while is used here to make sure the code is runs at lest once.
+        do
         {
-            //Sets the current wave that we are playing
-            currentWave = wave;
-
-            //Spawns all the enemies in that wave
-            for (int index = 0; index < currentWave.GetEnemyCount(); index++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                //Creates a game object from a prefab, in that position, with that rotation, and the specified parent
-                Instantiate(currentWave.GetEnemyPrefab(index), currentWave.GetStartingWaypoint().position, Quaternion.identity, transform);
-                //Waits for a random new spawn time before, instantiating another ship
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
-            }
+                //Sets the current wave that we are playing
+                currentWave = wave;
 
-            //Time before starting a new wave
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }         
+                //Spawns all the enemies in that wave
+                for (int index = 0; index < currentWave.GetEnemyCount(); index++)
+                {
+                    //Creates a game object from a prefab, in that position, with that rotation, and the specified parent
+                    Instantiate(currentWave.GetEnemyPrefab(index), currentWave.GetStartingWaypoint().position, Quaternion.identity, transform);
+                    //Waits for a random new spawn time before, instantiating another ship
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+
+                //Time before starting a new wave
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
+        }
+        while (isLooping);        
     }
 }
