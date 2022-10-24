cb8f1b9bfad64163aae1bb53c2e4e28ab0bb177f
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-10-24 14:14:31 -0300

    Added singleton behavior to AudioPlayer so that audio persists through the screens

--- Assets/Resources/Scripts/AudioPlayer.cs
+++ Assets/Resources/Scripts/AudioPlayer.cs
@@ -12,6 +12,34 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioClip takeDamageClip;
     [SerializeField] [Range(0f, 1f)] float takeDamageVolume = 1f;
 
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    //Responsible for keeping just one AudioPlayer for the game,
+    //that persists across all the screens
+    private void ManageSingleton()
+    {
+        //Gets all then instances of this script (class)
+        int instanceCount = FindObjectsOfType(GetType()).Length;
+        //If the second instance of this script appears,
+        //destroys it
+        if (instanceCount > 1)
+        {
+            //Disable it to prevent other areas of the game to access
+            //it the moment we are about to destroy it
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        //If this is the first instance of this script then mark
+        //it to not be destroyed when switching scenes
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     //Plays the shooting sound effect
     public void PlayShootingClip()
     {
